7bec69c3c78b898036a72a6a5b429535ba229edb
Christian Patrick Nebab <dongyang0216@gmail.com>
2024-02-11 19:16:09 +0800

    implement edit, update, and destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,46 @@
-
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+     @articles = Article.all
+  end
+  def show
+    find_article_params
+  end
+  def edit
+    find_article_params
+  end
+  def update
+    find_article_params
+    if @article.update(article_params)
 
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  def new
+    @article = Article.new
+  end
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+  def destroy
+    find_article_params
+    @article.destroy
+    redirect_to @article
+  end
+
+
+
+  private
+  def find_article_params
+    @article  = Article.find(params[:id])
+  end
+  def article_params
+    params.require(:article).permit(:title,:body)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,4 @@
 class Article < ApplicationRecord
-  validates :title, presence: true,length: { minimum: 10, maximum:100}
-  validates :writer_name, presence: true,length: { minimum: 6, maximum:50}
-  validates :body, presence: true,length: { minimum: 20, maximum:500}
+  validates :title, presence: true,length: { minimum: 6, maximum:100}
+  validates :body, presence: true,length: { minimum: 10, maximum:500}
 end
